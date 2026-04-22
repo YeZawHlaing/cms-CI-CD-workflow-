013d63c72d63cc20fc2abc31af01e3fa656093f6
yezawhlaing <yezawhlaing0@gmail.com>
2026-04-22 20:00:11 +0630

    redis template work perfectly

--- src/main/java/com/backend/cms/service/serviceImp/BlogServiceImp.java
+++ src/main/java/com/backend/cms/service/serviceImp/BlogServiceImp.java
@@ -7,10 +7,11 @@ import com.backend.cms.mapper.responseMapper.BlogResponseMapper;
 import com.backend.cms.model.Blog;
 import com.backend.cms.repo.BlogRepo;
 import com.backend.cms.service.BlogService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -21,22 +22,37 @@ public class BlogServiceImp implements BlogService {
     private final BlogRepo blogRepo;
     private final BlogRequestMapper requestMapper;
     private final BlogResponseMapper responseMapper;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private static final String BLOG_LIST_KEY = "blogs";
 
 
     @Override
     public BlogResponseDto createBlog(BlogRequestDto request) {
         Blog blog = requestMapper.toEntity(request);
-
         blog.setPost_date(LocalDateTime.now());
-
         Blog saved = blogRepo.save(blog);
 
+        redisTemplate.delete(BLOG_LIST_KEY);
         return responseMapper.toDto(saved);
     }
 
     @Override
     public List<BlogResponseDto> getAllBlogs() {
+
+        List<BlogResponseDto> cached =
+                (List<BlogResponseDto>) redisTemplate.opsForValue().get(BLOG_LIST_KEY);
+
+        if (cached != null) {
+            System.out.println("Blogs from Redis");
+            return cached;
+        }
+        System.out.println("Blogs from DB");
         List<Blog> blogs = blogRepo.findAll();
-        return responseMapper.toDtoList(blogs);
+        List<BlogResponseDto> response = responseMapper.toDtoList(blogs);
+
+        redisTemplate.opsForValue().set(BLOG_LIST_KEY, response, Duration.ofMinutes(5));
+
+        return response;
     }
 }
